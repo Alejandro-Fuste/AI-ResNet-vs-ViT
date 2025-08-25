530a7bf20f736a2c2e43c4ea788195b0884a6457
Alejandro F <afuste7@gmail.com>
2025-08-25 12:38:00 -0400

    add setup_conda.sh file to scripts folder. add code to setup_conda.sh file

---
+++ scripts/setup_conda.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+conda env create -f environment.yml || conda create -y -n cvsys python=3.10
+conda activate cvsys
+# If you created the env via create, install deps:
+pip install -r requirements.txt
